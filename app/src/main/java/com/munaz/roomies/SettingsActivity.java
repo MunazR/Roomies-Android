c43f5b122b6cec5553c0d22f736f0526a0bf0aca
Munaz <munaz_rahman@hotmail.com>
2016-03-11 10:38:32 -0500

    Fix issue with switching between activities

--- app/src/main/java/com/munaz/roomies/SettingsActivity.java
+++ app/src/main/java/com/munaz/roomies/SettingsActivity.java
@@ -89,7 +89,7 @@ public class SettingsActivity extends AppCompatActivity
             } else if (id == R.id.pantry) {
                 intent = new Intent(this, PantryActivity.class);
             } else if (id == R.id.roommates) {
-                intent = new Intent(this, SettingsActivity.class);
+                intent = new Intent(this, MainActivity.class);
             }
             intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(intent);
